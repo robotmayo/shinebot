612832a63e85bca202193bc7212c61c13c7c3e69
robotmayo <robotmayo@gmail.com>
2018-01-06 16:51:06 -0500

    make it the default export

--- src/middleware/make-middleware.ts
+++ src/middleware/make-middleware.ts
@@ -2,6 +2,6 @@ function _m(fns: any[], n: number, ...args: any[]) {
   if (n === fns.length - 1) return fns[n](...args);
   fns[n].apply(null, [...args, _m.bind(null, fns, n + 1)]);
 }
-export function middleware(...fns) {
+export default function middleware(...fns) {
   return _m.bind(null, fns, 0);
 }
